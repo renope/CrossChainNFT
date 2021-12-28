e52a2f3516872843e3c45e9e2976809a01acd21f
r.nouripoor <r.nouripoor@gmail.com>
2021-12-28 09:27:05 +0330

    pragma solidity ^0.8.2

--- contracts/CrossChainNFT/CrossChainNFT.sol
+++ contracts/CrossChainNFT/CrossChainNFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
