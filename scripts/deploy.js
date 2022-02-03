fd2d286695b837ed2f32efed93289ae64c99b884
r.nouripoor <r.nouripoor@gmail.com>
2022-02-03 20:05:21 +0330

    nothing

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,10 +9,20 @@ async function main() {
     // await CCN.deployed();
     // console.log("CrossChainNFT Contract Address:", CCN.address);
 
-    const CrossChainImplementation = await ethers.getContractFactory("CrossChainImplementation");
-    const CCI = await CrossChainImplementation.deploy();
-    await CCI.deployed();
-    console.log("CrossChainImplementation Contract Address:", CCI.address);
+    // const CrossChainImplementation = await ethers.getContractFactory("CrossChainImplementation");
+    // const CCI = await CrossChainImplementation.deploy();
+    // await CCI.deployed();
+    // console.log("CrossChainImplementation Contract Address:", CCI.address);
+
+    // const PublicNFT = await ethers.getContractFactory("PublicNFT");
+    // const PUB = await PublicNFT.deploy();
+    // await PUB.deployed();
+    // console.log("PublicNFT Contract Address:", PUB.address);
+
+    const Register = await ethers.getContractFactory("Register");
+    const R = await Register.deploy();
+    await R.deployed();
+    console.log("Register Contract Address:", R.address);
 }
   
 main()
